ee406fdd3ce64386de25b7a06ad2418821c9e174
HimekaidouHatate <yys14239102@gmail.com>
2021-05-10 19:28:27 +0900

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -4,10 +4,10 @@ AddClassPostConstruct("components/named_replica", function(self)
     local function OnNameDirty(inst)
         local name = inst.replica.named._name:value() -- a netvar sent from the host. 
         name = "Yakumo"
-        inst.name = name ~= "" and name or STRINGS.NAMES[string.upper(inst.prefab)] -- 
+        inst.name = name ~= "" and name or STRINGS.NAMES[string.upper(inst.prefab)]
     end
     
-    self.inst:RemoveAllEventCallbacks() -- We need to remove the event related namedirty but since replica's lister is only this one and I have no idea how to track this local function value with UpvalueHacker, remove all instead.
+    self.inst.event_listening.namedirty[self.inst] = nil -- remove old namedirty listener.
     
     if not GLOBAL.TheWorld.ismastersim then
         self.inst:ListenForEvent("namedirty", OnNameDirty) -- redefine event callback.
